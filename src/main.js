f94905e4ed6c0127e7c183d10902d4ff3a5739dd
luigicara <luigi.caravelli98@gmail.com>
2022-12-21 16:59:39 +0100

    imported @fontawesome and various icons

--- src/main.js
+++ src/main.js
@@ -2,4 +2,26 @@ import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faTwitter, faFacebookF, faLinkedin, faInstagram, faChevronDown)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')

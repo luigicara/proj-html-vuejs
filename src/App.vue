03809567f04bc597a567b5b23c42cd6452235f99
luigicara <luigi.caravelli98@gmail.com>
2022-12-21 13:03:14 +0100

    basic idea of structure + started AppHeader content

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
 <script>
+import { store } from './store';
 
+// importing components
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import MainAboutMe from './components/main_components/MainAboutMe.vue';
+import MainBegin from './components/main_components/MainBegin.vue';
+import MainBlogUpdated from './components/main_components/MainBlogUpdated.vue';
+import MainBookStore from './components/main_components/MainBookStore.vue';
+import MainRealStrories from './components/main_components/MainRealStrories.vue';
+import MainSponsor from './components/main_components/MainSponsor.vue';
+import MainStatistics from './components/main_components/MainStatistics.vue';
+
+export default {
+  components: {
+    MainStatistics,
+    AppHeader,
+    AppFooter,
+    MainAboutMe,
+    MainBegin,
+    MainBookStore,
+    MainSponsor,
+    MainRealStrories,
+    MainBlogUpdated
+  },
+  data() {
+    return {
+      store,
+    }
+  }
+}
 </script>
 
 <template>
-
+  <AppHeader />
+  <MainStatistics />
+  <MainAboutMe />
+  <MainBegin />
+  <MainBookStore />
+  <MainSponsor />
+  <MainRealStrories />
+  <MainBlogUpdated />
+  <AppFooter />
 </template>
 
 <style lang="scss">
+@use './style.scss' as *;
 
+.container {
+  max-width: 1170px;
+  margin: 0 auto;
+}
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    data() {
+        return {
+            navElements: ['Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop']
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../partials/variables' as *;
+
+header {
+    background-color: $primaryBackground;
+    background-image: url('../../public/background-pattern-wavify.png');
+    height: 800px;
+}
+</style>

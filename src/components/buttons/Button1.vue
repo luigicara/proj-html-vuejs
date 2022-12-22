fa80fbd2a261aa378b2404a4499c5cbec2612c98
luigicara <luigi.caravelli98@gmail.com>
2022-12-22 13:13:56 +0100

    made button as a component

---
+++ src/components/buttons/Button1.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    props: {
+        msg: String,
+    }
+}
+</script>
+
+<template>
+    <button>
+        {{ msg }} &nbsp;
+        <font-awesome-icon icon="fa-solid fa-arrow-right-long" />
+    </button>
+</template>
+
+<style lang="scss" scoped>
+@use 'src/partials/_variables.scss' as *;
+
+button {
+    margin: 1.5rem 0;
+    padding: 1rem 3.5rem;
+    border: none;
+    border-radius: 0.3rem;
+    color: white;
+    font-weight: bold;
+    background-color: $secondaryText;
+
+    &:hover {
+        background-color: $primaryText;
+    }
+}
+</style>

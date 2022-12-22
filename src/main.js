2473faf0504a1d35e1b28ce2a8cf46b2d2822c3b
luigicara <luigi.caravelli98@gmail.com>
2022-12-22 13:13:03 +0100

    imported other icons

--- src/main.js
+++ src/main.js
@@ -19,8 +19,16 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
+
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+
+import { faFileLines } from '@fortawesome/free-regular-svg-icons'
+
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+
 /* add icons to the library */
-library.add(faTwitter, faFacebookF, faLinkedin, faInstagram, faChevronDown)
+library.add(faTwitter, faFacebookF, faLinkedin, faInstagram, faChevronDown, faArrowRightLong, faUser, faFileLines, faCheck)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)

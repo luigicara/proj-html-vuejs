f96bdbb31b9bafdb19c999e2adf95153cd6c570d
luigicara <luigi.caravelli98@gmail.com>
2022-12-22 13:16:07 +0100

    BookStore info section complete

--- src/components/main_components/MainBookStore.vue
+++ src/components/main_components/MainBookStore.vue
@@ -1,13 +1,96 @@
 <script>
-export default {
+import Button1 from '../buttons/Button1.vue';
 
+export default {
+    data() {
+        return {
+            books: [
+                {
+                    img: "11",
+                    price: "29.00",
+                    name: "Aplha man by Maxcoach",
+                },
+                {
+                    img: "10",
+                    price: "39.00",
+                    name: "Real man 4.0 by Maxcoach",
+                }
+            ],
+            features: [
+                "Help you understand yourself better",
+                "Revealing mature tips",
+                "Give the right advice",
+                "Fascinating examples of alpha man"
+            ]
+        };
+    },
+    components: { Button1 }
 }
 </script>
 
 <template>
+    <div class="container">
+        <div class="info">
+            <div class="title">BOOK STORE ONLINE</div>
 
+            <h2>
+                Be Alpha With
+                <span>
+                    Wingsman's Book
+                </span>
+            </h2>
+
+            <ul>
+                <li v-for="feature in features">
+                    <span>
+                        <font-awesome-icon icon="fa-solid fa-check" /> &nbsp;
+                    </span>
+                    {{ feature }}
+                </li>
+            </ul>
+
+            <Button1 :msg="'Get Free Ebook'" />
+        </div>
+
+        <div class="book">
+
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+@use 'src/partials/_variables.scss' as *;
 
+.container {
+    display: flex;
+    margin: 5rem auto;
+
+    .info {
+        width: 30%;
+
+        h2 {
+            font-size: 2.5rem;
+            color: $primaryText;
+            margin: 1rem 0 2rem;
+
+            span {
+                color: $secondaryText;
+            }
+        }
+
+        ul {
+            color: $tertiaryText;
+
+            li {
+                font-size: 0.9rem;
+                margin: 0.8rem 0;
+            }
+
+            span {
+                color: $secondaryText;
+                font-size: 1rem;
+            }
+        }
+    }
+}
 </style>

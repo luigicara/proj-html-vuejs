98f678d8ce2e97fe2c60449a95932eb57c67ed9c
luigicara <luigi.caravelli98@gmail.com>
2022-12-22 15:57:40 +0100

    complete Sponsor section

--- src/components/main_components/MainSponsor.vue
+++ src/components/main_components/MainSponsor.vue
@@ -1,13 +1,36 @@
 <script>
 export default {
-
+    data() {
+        return {
+            imgStrings: ['05', '06', '07', '04', '03', '02']
+        }
+    }
 }
 </script>
 
 <template>
-
+    <div class="container">
+        <div v-for="img in imgStrings">
+            <img :src="`client-logo-${img}-primary.png`" alt="sponsor">
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+.container {
+    display: flex;
+    gap: 1rem;
+    margin: 7rem auto;
 
+    div {
+        width: calc((100% - 5rem) / 6);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    img {
+        filter: saturate(40%) opacity(30%);
+    }
+}
 </style>
